4f5bcc103bfd37d97f5b627cc4dbc9221216c07c
wakio-ngari <ngarijoyce017@gmail.com>
2025-02-14 11:56:55 +0300

    completed attributes ans properties lab

--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python3
 
 APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
+    "Admin", "Customer Service", "Human Resources", "ITC", "Production",
+    "Legal", "Finance", "Sales", "General Management", "Research & Development",
+    "Marketing", "Purchasing"
 ]
 
 class Person:
-    pass
+    def __init__(self, name="John Doe", job="Unemployed"):  
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title() 
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
